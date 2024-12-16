3e57692c0130cb2a09b4c991d190caaef7970980
Glen Marti <martidicianolold@gmail.com>
2024-12-16 11:25:14 +0100

    useReduce

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import './App.css';
+import "./App.css";
+import Counter from "./state-management/Counter";
+import TaskList from "./state-management/TaskList";
 
 function App() {
-  return <h1>React Starter Project</h1>;
+  return <TaskList />;
 }
 
 export default App;
